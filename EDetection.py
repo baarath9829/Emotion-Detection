2383acdd32964df21062b1158ee96b248400b138
baarath <baarath.s.2016.cse@rajalakshmi.edu.in>
2019-01-30 23:17:41 +0530

    second commit conflict in passing img as argument distance is caculated using euclidean formula

--- EDetection.py
+++ EDetection.py
@@ -1,26 +1,35 @@
 import dlib
 import cv2
 import numpy
-def plot(image,landmark):
+def plot(image,landmark): #passing img AS argument causing problem
     image_c = image.copy
     for point in landmark:
         pos = (point[0,0],point[0,1])
+        print pos
         cv2.circle(image_c, pos, 3, color=(0, 255, 0))
     return image_c
         
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 win = dlib.image_window()
-img = cv2.imread("test1.jpg",0);
+img = cv2.imread("testface.jpg",1);
+image = img.copy()
 win.set_image(img)
 dets = detector(img,1)
 for k, d in enumerate(dets):
     shape = predictor(img,d)
-    #print("Part 0: {}, Part 1: {} part2 : {}".format(shape.part(0),shape.part(1),shape.part(2)))
+    print("Part 0: {}, Part 1: {} part2 : {}".format(shape.part(0),shape.part(1),shape.part(2)))
     mat = numpy.matrix([[p.x, p.y] for p in shape.parts()])
-    win.add_overlay(shape)
-    #plottedImg = plot(img,mat)
     
+    win.add_overlay(shape)
+    #plottedImg = plot(image,mat)
+    
+for point in mat:
+        pos = (point[0,0],point[0,1])
+        print pos
+        cv2.circle(image, pos, 3, (0, 255, 0),-1)    
 win.add_overlay(dets)
-#cv2.imshow("landmark", plottedImg)
+cv2.imshow("landmark", image)
 dlib.hit_enter_to_continue()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
